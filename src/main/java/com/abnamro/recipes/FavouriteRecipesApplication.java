bff7bf11d04adec02e2fa1e79fe9110c3601849a
hippalus <habiphakan.isler@gmail.com>
2023-06-15 00:35:12 +0200

    initial commit

---
+++ src/main/java/com/abnamro/recipes/FavouriteRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.abnamro.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteRecipesApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(FavouriteRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abnamro/recipes/FavouriteRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnamro.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouriteRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
